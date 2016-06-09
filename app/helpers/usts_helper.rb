600c8ab9ef5e8ed6a00c7d467e7c5e1cf41b2df6
Matthieu Tanguay-Carel <matthieutc@gmail.com>
2016-06-09 22:19:18 +0200

    applies fix to sharp too

--- app/helpers/usts_helper.rb
+++ app/helpers/usts_helper.rb
@@ -1,6 +1,6 @@
 module USTsHelper
   def fix_letter_spacing obj
     string = obj.to_s
-    string.gsub("♭", "<span class='narrow'>♭</span>").html_safe
+    string.gsub("♭", "<span class='narrow'>♭</span>").gsub("♯", "<span class='narrow'>♯</span>").html_safe
   end
 end
