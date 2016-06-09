11715ae1e98d0ca7ccad63544a8181a61138b6ea
Matthieu Tanguay-Carel <matthieutc@gmail.com>
2016-06-09 22:17:18 +0200

    improves font rendering on phone

--- app/helpers/usts_helper.rb
+++ app/helpers/usts_helper.rb
@@ -1,2 +1,6 @@
 module USTsHelper
+  def fix_letter_spacing obj
+    string = obj.to_s
+    string.gsub("♭", "<span class='narrow'>♭</span>").html_safe
+  end
 end
--- app/models/ust.rb
+++ app/models/ust.rb
@@ -15,7 +15,7 @@ class UST
     keys = %w(A B♭ B C D♭ D E♭ E F G♭ G A♭)
     usts = {}
     usts["7"] = ["♭9♭13", "♯11", "♯9", "♭5♭9", "♯9♭13", "♭9", "sus4"] if @dominant_enabled
-    usts["▵"] = ["♯11", "9"] if @major_enabled
+    usts["△"] = ["♯11", "9"] if @major_enabled
     usts["-7"] = ["13", "♭13"] if @minor_enabled
     usts["-7♭5"] = ["♭13", "11"] if @minor_flat_five_enabled
     usts["º"] = ["13", "♯11"] if @diminished_enabled
