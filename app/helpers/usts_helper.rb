1cf979eec0e65ff9f8c521063d9d9b2ec09c8256
matt <matthieutc@gmail.com>
2016-08-27 13:30:34 +0200

    adds font fixes back

--- app/helpers/usts_helper.rb
+++ app/helpers/usts_helper.rb
@@ -1,6 +1,6 @@
 module USTsHelper
   def fix_letter_spacing obj
     string = obj.to_s
-    string.gsub(" ", "&thinsp;").html_safe
+    string.gsub(" ", "&thinsp;").gsub("♭", "<span class='narrow'>♭</span>").gsub("♯", "<span class='narrow'>♯</span>").gsub("△", "<span class='maj7'>△</span>").html_safe
   end
 end
